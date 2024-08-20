0cbd9f1d6273c8ff47bfb8ee9396ecd1c43091b0
JakobPalomo <jakobmichael.palomo.cics@ust.edu.ph>
2024-08-20 22:53:56 +0800

    login route

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 // Material Kit 2 React themes
 import theme from "assets/theme";
 import Presentation from "layouts/pages/presentation";
+import LoginModal from "pages/LandingPages/LoginMod";
 
 // Material Kit 2 React routes
 import routes from "routes";
@@ -57,6 +58,7 @@ export default function App() {
       <Routes>
         {getRoutes(routes)}
         <Route path="/presentation" element={<Presentation />} />
+        <Route path="/login" element={<LoginModal />} />
         <Route path="*" element={<Navigate to="/presentation" />} />
       </Routes>
     </ThemeProvider>
